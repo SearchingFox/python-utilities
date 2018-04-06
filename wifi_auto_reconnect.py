9f2d1396660631710c315588172f68d21021190d
ASNordroid <n3tstrang3r@yandex.ru>
2018-04-06 04:53:20 +0300

    Add script for parsing firefox bokmarks

--- wifi_auto_reconnect.py
+++ wifi_auto_reconnect.py
@@ -2,7 +2,7 @@
 from subprocess import check_output
 
 wlan_name = "zzznet"
-connectedQ = (check_output("netsh wlan show interfaces", shell=True).decode('ascii', 'ignore')).find(wlan_name)
+connectedQ = check_output("netsh wlan show interfaces", shell=True).decode('ascii', 'ignore').find(wlan_name)
 
 if connectedQ < 0:
     out = check_output("netsh wlan connect " + wlan_name, shell=True)
