d9f5ce5d58eb2e71ee87cd6d42f59c763228ca88
ASNordroid <n3tstrang3r@yandex.ru>
2018-04-08 09:55:12 +0300

    Add find_duplicates function in firefox_bookmarks.py

--- firefox_bookmarks.py
+++ firefox_bookmarks.py
@@ -14,62 +14,37 @@ def stripe_images(old_file_path):
 # find duplicate bookmarks in firefox export file
 def find_duplicates(file_path):
     links = {}
-    dups  = []
-    with open(file_path, 'r', encoding='utf-8') as file:
-        all_ = file.readlines()
-        links0 = [i[i.find('"')+1:i.find('" A')] for i in all_ if '<A' in i and "<H3" not in i]
+    dupls = []
+    count = 0
 
-        for line in all_:
+    with open(file_path, 'r', encoding='utf-8') as bookmarks_file:
+        for line in bookmarks_file.readlines():
             if "<H3" in line:
                 folder = line[line.find("\">")+2:line.find("</")]
             elif '<A' in line:
-                addr = line[line.find('"')+1:line.find('" A')]
-                if addr not in links.keys():
+                count += 1
+                addr = '/'.join(line[line.find('"')+1:line.find('" A')].split('/')[2:]) if line.startswith("http") else line[line.find('"')+1:line.find('" A')]
+
+                if addr not in links:
                     links[addr] = []
-                elif addr not in dups:
-                    dups.append(addr)
+                elif addr not in dupls:
+                    dupls.append(addr)
 
                 links[addr].append(folder)
-    
-    # dups0 = []
-    # for pos_1, lnk_1 in enumerate(links0):
-    #     for pos_2, lnk_2 in enumerate(links0):
-    #         if pos_1 != pos_2 and lnk_1 == lnk_2 and lnk_1 not in dups0:
-    #             dups0.append(lnk_1)
 
-    # if dups0 != dups:
-    #     print("ERROR!!!")
-    #     if len(dups0) > len(dups):
-    #         for i in dups0:
-    #             if i not in dups:
-    #                 print(i)
-    #     else:
-    #         for i in dups:
-    #             if i not in dups0:
-    #                 print(i)
-        print("all links:", len(links0))
-        
-        print("duplicates:", len(dups))
-        dups2 = []
-        for i in links.values():
-            if len(i) > 1:
-                dups2.append(i)
-        print("duplicates2:", len(dups2))
-        print(len(links.keys()))
-        for i in links0:
-            if i not in links.keys():
-                print(i)
-    # for i in sorted(dups):
+    print("all links:", count)
+    print("duplicates:", len(dupls))
+    # for i in sorted(dupls):
     #     print(i, links[i])
-
-    # with open("dup.txt", 'w') as file:
-    #     file.write('\n'.join(dup))
+    
+    with open("dupls.txt", 'w') as dupls_file:
+        dupls_file.write('\n'.join(sorted(dupls)))
 
 
 if len(sys.argv) > 1:
     file = os.path.join(os.getcwd(), sys.argv[2])
 else:
-    htmls = [i for i in os.listdir() if i.endswith(".html") and "firefox" in i]
+    htmls = [i for i in os.listdir() if i.endswith(".html") and i.startswith("bookmarks_firefox")]
     if len(htmls) == 1:
         file = os.path.join(os.getcwd(), htmls[0])
     else:
